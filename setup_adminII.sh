adefcb494a30a7f95e1c3d34d3810448244def61
mkiser8103 <mkiser8103@gmail.com>
2016-07-07 16:25:20 -0400

    Create setup_adminII.sh

---
+++ setup_adminII.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+set -o xtrace
+
+export HOME=${HOME:-/root}
+export TERM=xterm
+export ambari_pass=${ambari_pass:-BadPass#1}
+export ambari_server_custom_script=${ambari_server_custom_script:-~/ambari-bootstrap/ambari-extras.sh}
+
+cd
+
+yum makecache
+yum -y -q install git epel-release ntpd screen mysql-connector-java jq python-argparse python-configobj
+
+el_version=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release | cut -d. -f1)
+case ${el_version} in
+  "6")
+    sed -i "s/mirrorlist=https/mirrorlist=http/" /etc/yum.repos.d/epel.repo || true
+  ;;
+  "7")
+    rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+  ;;
+esac
+
+curl -sSL https://raw.githubusercontent.com/HortonworksUniversity/ambari-bootstrap/master/extras/deploy/install-ambari-bootstrap.sh | bash
+
+~/ambari-bootstrap/ambari-bootstrap.sh
+
+## Ambari Server specific tasks
+if [ "${install_ambari_server}" = "true" ]; then
+    bash -c "nohup ambari-server start" || true
+
+    sleep 60
+
+    ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar
+    ambari_pass=admin source ~/ambari-bootstrap/extras/ambari_functions.sh
+    ambari-change-pass admin admin ${ambari_pass}
+
+    if [ "${deploy}" = "true" ]; then
+
+        cd ~/ambari-bootstrap/deploy
+
+        ## various configuration changes for demo environments, and fixes to defaults
+cat << EOF > configuration-custom.json
+{
+  "configurations" : {
+    "yarn-site": {
+        "yarn.scheduler.minimum-allocation-vcores": "1",
+        "yarn.scheduler.maximum-allocation-vcores": "1",
+        "yarn.scheduler.minimum-allocation-mb": "256",
+        "yarn.scheduler.maximum-allocation-mb": "2048"
+    },
+    "hive-site": {
+        "hive.support.concurrency": "true",
+        "hive.enforce.bucketing": "true",
+        "hive.exec.dynamic.partition.mode": "nonstrict",
+        "hive.txn.manager": "org.apache.hadoop.hive.ql.lockmgr.DbTxnManager",
+        "hive.compactor.initiator.on": "true",
+        "hive.compactor.worker.threads": "1"
+    },
+    "solr-env": {
+        "solr.port": "6083"
+    },
+    "solr-config": {
+        "solr.port": "6083",
+        "solr.datadir": "/opt/ranger_audit_server",
+        "solr.download.location": "HDPSEARCH",
+        "solr.znode": "/ranger_audits",
+        "solr.cloudmode": "true"
+    }
+  }
+}
+EOF
+
+        export ambari_services="${ambari_services:-"HDFS MAPREDUCE2 PIG YARN HIVE ZOOKEEPER AMBARI_METRICS"}"
+        export ambari_password="${ambari_pass}"
+        export cluster_name=${stack:-mycluster}
+        export host_count=${host_count:-skip}
+
+        ./deploy-recommended-cluster.bash
+        cd ~
+        sleep 5
+
+        source ~/ambari-bootstrap/extras/ambari_functions.sh
+        ambari-configs
+        ambari_wait_request_complete 1
+    fi
+fi
+
+exit 0
